3eaebae30e02761a545025d22a4419f4f0ebe457
Alex Knopach <alexanderknopach@gmail.com>
2021-05-08 01:47:06 +0300

    feat: implement numberConverter: method

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
@@ -3,7 +3,23 @@
 @implementation NumberConverter
 
 - (NSArray *)numberConverter:(NSNumber *)number {
-    return nil;
-}
+    if (!number) {
+        return @[];
+    }
+    
+    NSString *stringFromNumber = [number stringValue];
+    stringFromNumber = [stringFromNumber stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"-"]];
+    
+    NSMutableArray *arrayOfChars = [[NSMutableArray alloc] init];
+    
+    for (int i = 0; i < [stringFromNumber length]; i++) {
+        [arrayOfChars addObject:[NSString stringWithFormat:@"%C", [stringFromNumber characterAtIndex:i]]];
+    }
+    
+    NSEnumerator *enumer = [arrayOfChars reverseObjectEnumerator];
+    
+    NSArray *enumeratedArray = [enumer allObjects];
+    
+    return enumeratedArray;}
 
 @end
