1127eca771fec3dc320173a32badc70bdbb48c13
Alex Knopach <alexanderknopach@gmail.com>
2021-05-07 11:24:11 +0300

    feat: implement isValidNumber getter

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-2/NSString+ValidNumber.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-2/NSString+ValidNumber.m
@@ -3,7 +3,11 @@
 @implementation NSString (ValidNumber)
 
 - (BOOL)isValidNumber {
-    return false;
+    
+    NSCharacterSet *letters = [[NSCharacterSet decimalDigitCharacterSet] invertedSet];
+    NSRange letterRange = [self rangeOfCharacterFromSet:letters];
+    
+    return letterRange.location == NSNotFound && self.length > 0;
 }
 
 @end
