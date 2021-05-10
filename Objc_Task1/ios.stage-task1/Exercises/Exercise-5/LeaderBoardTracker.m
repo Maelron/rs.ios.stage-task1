06e3828d311758bc2b971c7f36752166db55f9b1
Alex Knopach <alexanderknopach@gmail.com>
2021-05-10 13:42:43 +0300

    feat: implement trackLeaderBoard:PlayerArray: method

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-5/LeaderBoardTracker.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-5/LeaderBoardTracker.m
@@ -10,7 +10,46 @@
 
 - (NSArray <NSNumber *> *)trackLeaderBoard:(NSArray <NSNumber *> *)rankedArray
                                playerArray:(NSArray <NSNumber *> *)playerArray {
-    return @[];
+    
+    NSMutableArray *resultsArray = [[NSMutableArray alloc] init];
+    
+    if ([rankedArray isEqualToArray:@[]]) {
+        for (int i = 0; i < playerArray.count; i++) {
+            [resultsArray addObject:@1];
+        }
+        return resultsArray;
+    }
+    
+    if ([playerArray isEqualToArray:@[]]) {
+        return @[];
+    }
+    
+    NSMutableArray *uniqueArr = [[NSMutableArray alloc] initWithArray:rankedArray];
+    for (NSUInteger i = rankedArray.count - 1; i > 0; i--) {
+        if ([rankedArray[i] isEqual: rankedArray[i-1]]) {
+            [uniqueArr removeObjectAtIndex: i];
+        }
+    }
+    
+    for (int i = 0; i < playerArray.count; i++) {
+        NSInteger placeResult = 1;
+        
+        for (int j = 0; j < uniqueArr.count; j++) {
+            if (playerArray[i].integerValue >= [uniqueArr[j] integerValue]) {
+                placeResult += 0;
+            }
+            
+            if (playerArray[i].integerValue < [uniqueArr[j] integerValue]) {
+                placeResult += 1;
+                
+            }
+        }
+        [resultsArray addObject:[NSNumber numberWithInteger:placeResult]];
+        placeResult += 1;
+        
+    }
+    return resultsArray;
+    
 }
 
 @end
